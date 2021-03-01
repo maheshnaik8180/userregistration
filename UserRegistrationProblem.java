768845da39561edade6de931dec9e6c7aaabcdb4
unknown <naikmahesh.naik@gmail.com>
2021-03-01 22:55:21 +0530

    check valid email

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -1,44 +1,24 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistrationProblem
-{
-
-public static void firstNameCheck(String name){ //check if fname i/p match with pattern
-	String regex = "^[A-Z]{1}[a-zA-Z]{2,}$"; //1st letter is capital min 3 letter
-
-	Pattern pattern = Pattern.compile(regex);
-	Matcher match = pattern.matcher(name);
-	boolean bool = match.matches(); //check pattern match and give return boolen value
-	if (bool) 
-			System.out.println(name + " is a valid first name");
-		else
-			System.out.println(name + " is not a valid first name");
+public class UserRegistrationProblem {
+	static Scanner sc = new Scanner(System.in);
+	public static void validation(String firstName, String lastName, String email) {
+		// First Name and Last Name comes with restriction such as first letter should be upper case and with minimum 3 characters
+		System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
+		System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
+	//  abc.xyz@bl.co.in is valid with abc bl and co are mandatory 2 optional(xyz,in) with precise @ and . positions 
+		System.out.println("E-Mail: " + Pattern.matches("^[a-zA-Z0-9]{3,}([.]{1}+[a-zA-Z0-9]{3,})*[@]{1}[a-zA-Z0-9]{2,}[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z0-9]{2,})*$", email));
 	}
-	
-
-public static void lastNameCheck(String name) {
-	String regex = "^[A-Z]{1}[a-zA-Z]{2,}$"; // 1st letter is Capital and has minimum of 3 letters
-	Pattern pattern = Pattern.compile(regex);
-	Matcher match = pattern.matcher(name);
-	boolean bool = match.matches();// checks if the pattern matches with the given pattern and returns a boolean value 
-		if (bool) 
-			System.out.println(name + " is a valid last name");
-		else
-			System.out.println(name + " is not a valid last name");
-}
-
-	public static void main(String[] args)
-	{
-	System.out.println("Enter first Name");
-	Scanner sc = new Scanner(System.in);
-	
-	String firstName= sc.next();
-	System.out.println("Enter a valid last name");
-	String lastName = sc.next();
-	firstNameCheck(firstName);
-	lastNameCheck(lastName);
-
-}
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Program");
+		System.out.println("Enter a valid first name");
+		String firstName = sc.nextLine();
+		System.out.println("Enter a valid last name");
+		String lastName = sc.nextLine();
+		System.out.println("Enter a valid email");
+		String email = sc.nextLine();
+		validation(firstName, lastName, email);
+		
+	}
 }
