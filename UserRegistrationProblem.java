cb852cfb24419c5eb1dcaee5d2428b8bf55e3241
unknown <naikmahesh.naik@gmail.com>
2021-03-01 23:35:47 +0530

    check valid password Rule1

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationProblem {
 	static Scanner sc = new Scanner(System.in);
-	public static void validation(String firstName, String lastName, String email, String mobile) {
+	public static void validation(String firstName, String lastName, String email, String mobile, String password) {
 		// First Name and Last Name comes with restriction such as first letter should be upper case and with minimum 3 characters
 		System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
 		System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
@@ -11,6 +11,7 @@ public class UserRegistrationProblem {
 		System.out.println("E-Mail: " + Pattern.matches("^[a-zA-Z0-9]{3,}([.]{1}+[a-zA-Z0-9]{3,})*[@]{1}[a-zA-Z0-9]{2,}[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z0-9]{2,})*$", email));
 		// 91 followed by space followed by 10 digit number
 		System.out.println("Mobile Number: " + Pattern.matches("^[9][1][\s][6-9][0-9]{9}$", mobile));
+		System.out.println("password: " + Pattern.matches("^[a-zA-Z0-9]{8,}$", password));
 	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to User Registration Program");
@@ -22,6 +23,8 @@ public class UserRegistrationProblem {
 		String email = sc.nextLine();
 		System.out.println("Enter a valid mobile number starting with 91 folowwed by a space and 10 didit number");
 		String mobile = sc.nextLine();
-		validation(firstName, lastName, email, mobile);
+		System.out.println("Enter a valid password with minimum 8 charcters");
+		String password = sc.nextLine();
+		validation(firstName, lastName, email, mobile, password);
 	}
 }
